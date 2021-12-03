cc291791785419d63b6dc6f12135c7a3fbcc355f
Jacob Harrington <jacob@go.social>
2021-12-03 15:01:16 -0800

    sort by name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import SearchForm from './Components/SearchForm';
 import List from './Components/List';
-import { getStarWarsPeople } from './services';
+import { getStarWarsPeople, sortByName } from './services';
 
 function App() {
   const [ people, setPeople ] = useState([]);
@@ -9,14 +9,14 @@ function App() {
   useEffect(() => {
     getStarWarsPeople()
       .then(res => {
-        setPeople(res.results);
+        setPeople(sortByName(res.results));
       });
   }, []);
 
   const getPeopleWithName = (name) => {
     return getStarWarsPeople(name)
       .then(res => {
-        setPeople(res.results);
+        setPeople(sortByName(res.results));
       });
   };
 
--- src/services.js
+++ src/services.js
@@ -1,5 +1,11 @@
 const url = 'https://swapi.dev/api/people';
 
+export const sortByName = (people) => people.sort((a, b) => {
+  if(a.name < b.name) return -1;
+  if(a.name > b.name) return 1;
+  return 0;
+});
+
 export const getStarWarsPeople = (name) => {
   if(name) return fetch(`${url}?search=${name.trim().toLowerCase()}`).then(people => people.json());
   
