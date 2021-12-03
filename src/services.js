ac5f6ccdb4cf56accf1c16ff75b740e1bc6b9365
Jacob Harrington <jacob@go.social>
2021-12-03 14:16:56 -0800

    initial scaffolding - service created

---
+++ src/services.js
@@ -0,0 +1,7 @@
+const url = 'https://swapi.dev/api/people?search=han';
+
+export const getStarWarsPeople = (name) => {
+  if(name) return fetch(`${url}?search=${name.trim().toLowerCase()}`).then(people => people.json());
+  
+  return fetch(url).then(people => people.json());
+}
